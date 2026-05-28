15774305ccfda09ae81d2675676e5e9883fb1b18
Zedrik Cayne <zedrikcayne@gmail.com>
2026-05-27 23:07:55 -0400

    Initial release...good'nuff for government work.

---
+++ README.md
@@ -0,0 +1,5 @@
+More dogfood for crankshaft.
+
+Since I'm making more and more tiny things... figure'd I'd put a front end on them all.
+
+Also where I'll probably add rate limiting and what not.
--- runSecure.sh
+++ runSecure.sh
@@ -1,5 +1,5 @@
 
 #gdb --args build/handle --trace --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access logs/access
-build/handle --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access logs/access
+build/handle --info --port 8443 --key secrets/key.pem --certificate secrets/certificate.pem --log-access logs/access
 
 
